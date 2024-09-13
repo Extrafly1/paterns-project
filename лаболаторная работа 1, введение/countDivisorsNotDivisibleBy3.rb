5f577259084e214ce60a891b596fba77cec897a8
Extrafly1 <extrafly555367@gmail.com>
2024-09-13 22:42:33 +0300

    commit 3

---
+++ лаболаторная работа 1, введение/countDivisorsNotDivisibleBy3.rb
@@ -0,0 +1,14 @@
+                                                # Первый метод четвёртого задания
+def countDivisorsNotDivisibleBy3(number)        # функция подсчета делителей
+  count = 0
+  (1..number).each do |i|                       # перебираем числа от 1 до number
+    if number % i == 0 && i % 3 != 0            # если делится и не делится на 3
+      count += 1
+    end
+  end
+  count                                         # возвращаем результат
+end
+
+puts "Введите целое положительное число: "
+number = gets.to_i                              # вводим число
+puts "Количество делителей числа, не делящихся на 3: #{countDivisorsNotDivisibleBy3(number)}"
---
+++ лаболаторная работа 1, введение/sumOfDivisorsRelativelyPrime.rb
@@ -0,0 +1,23 @@
+                                                                          # Третий метод четвёртого задания
+def gcd(a, b)                                                             # функция нахождения НОД
+  b.zero? ? a : gcd(b, a % b)                                             # если второй числитель равен нулю, возвращаем первый
+end
+
+def sumOfDivisorsRelativelyPrime(number)                                  # функция подсчета суммы делителей
+  sum_of_digits = number.to_s.chars.map(&:to_i).sum                       # сумма цифр
+  product_of_digits = number.to_s.chars.map(&:to_i).reduce(1, :*)         # произведение цифр
+
+  sum = 0
+  (1..number).each do |i|                                                 # перебираем числа от 1 до number
+    if number % i == 0                                                    # если делится
+      if gcd(i, sum_of_digits) == 1 && gcd(i, product_of_digits) != 1     # взаимно простые
+        sum += i                                                          # суммируем
+      end
+    end
+  end
+  sum                                                                     # возвращаем результат
+end
+
+puts "Введите целое положительное число: "
+number = gets.to_i                                                        # вводим число
+puts "Сумма делителей, взаимно простых с суммой цифр и не взаимно простых с произведением цифр: #{sumOfDivisorsRelativelyPrime(number)}"
