28b3fbe3658b78f9bfa522c6063bc8b3a380b811
Efl <133080730+Extrafly1@users.noreply.github.com>
2024-09-14 13:39:22 +0300

    update name function

--- лаболаторная работа 1, введение/countDivisorsNotDivisibleBy3.rb
+++ лаболаторная работа 1, введение/countDivisorsNotDivisibleBy3.rb
@@ -1,5 +1,5 @@
                                                 # Первый метод четвёртого задания
-def countDivisorsNotDivisibleBy3(number)        # функция подсчета делителей
+def count_divisors_not_divisible_by_3(number)        # функция подсчета делителей
   count = 0
   (1..number).each do |i|                       # перебираем числа от 1 до number
     if number % i == 0 && i % 3 != 0            # если делится и не делится на 3
@@ -11,4 +11,4 @@ end
 
 puts "Введите целое положительное число: "
 number = gets.to_i                              # вводим число
-puts "Количество делителей числа, не делящихся на 3: #{countDivisorsNotDivisibleBy3(number)}"
+puts "Количество делителей числа, не делящихся на 3: #{count_divisors_not_divisible_by_3(number)}"
