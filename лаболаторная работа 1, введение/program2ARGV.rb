125bda16c1a5699365d02b2bfb8da80ecaed4e8d
Efl <133080730+Extrafly1@users.noreply.github.com>
2024-09-14 13:50:22 +0300

    Update functions name and variebles name

--- лаболаторная работа 1, введение/program2ARGV.rb
+++ лаболаторная работа 1, введение/program2ARGV.rb
@@ -1,4 +1,4 @@
-def findMinimum(array)                                                                  # найти минимальный элемент в массиве
+def find_minimum(array)                                                                  # найти минимальный элемент в массиве
   min = array[0]                                                                        # принимаем первый элемент как минимальный
   array.each do |i|                                                                     # перебираем остальные
     min = i if i < min                                                                  # если элемент меньше минимального
@@ -6,7 +6,7 @@ def findMinimum(array)                                                          
   min                                                                                   # возвращаем минимальный
 end
 
-def findFirstPositiveIndex(array)                                                       # найти первый положительный элемент в массиве
+def find_first_positive_index(array)                                                       # найти первый положительный элемент в массиве
   array.each_with_index do |value, index|                                               # перебираем остальные
     return index if value > 0                                                           # если элемент положительный
   end
@@ -21,11 +21,11 @@ if ARGV.length != 2                                                             
 end
 
 method = ARGV[0]                                                                        # метод
-filePath = ARGV[1]                                                                      # путь к файлу
+file_path = ARGV[1]                                                                      # путь к файлу
 
                                                                                         # Чтение массива из файла
 begin
-  array = File.readlines(filePath).map(&:to_i)                                          # считываем массив из файла
+  array = File.readlines(file_path).map(&:to_i)                                          # считываем массив из файла
 rescue Errno::ENOENT                                                                    # если не удалось открыть файл
   puts "Ошибка: Файл не найден."
   exit                                                                                  # выходим из программы
@@ -34,14 +34,14 @@ end
                                                                                         # Выполнение выбранного метода
 case method                                                                             # выбор метода
 when 'min' 
-  minValue = findMinimum(array)                                                         # найти минимальный элемент в массиве
-  puts "Минимальный элемент: #{minValue}"                                               # вывод на экран
+  min_value = find_minimum(array)                                                         # найти минимальный элемент в массиве
+  puts "Минимальный элемент: #{min_value}"                                               # вывод на экран
 when 'firstPositive'
-  firstPositiveIndex = findFirstPositiveIndex(array)                                    # найти первый положительный элемент в массиве
-  if firstPositiveIndex.nil?                                                            # если положительных элементов нет
+  first_positive_index = find_first_positive_index(array)                                    # найти первый положительный элемент в массиве
+  if first_positive_index.nil?                                                            # если положительных элементов нет
     puts "Положительных элементов нет."
   else
-    puts "Номер первого положительного элемента: #{firstPositiveIndex}"
+    puts "Номер первого положительного элемента: #{first_positive_index}"
   end
 else                                                                                    # если неизвестный метод
   puts "Ошибка: Неизвестный метод '#{method}'. Используйте 'min' или 'firstPositive'."
