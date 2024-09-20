75c733fa59501f4ea7fc5ef526b4bfce1414df2a
Extrafly1 <extrafly555367@gmail.com>
2024-09-21 02:35:22 +0300

    lab2_work2_task4_add_Student_short_class

--- lab_work_2/student_class.rb
+++ lab_work_2/student_class.rb
@@ -152,3 +152,45 @@ class Student
     contact_info
   end
 end
+
+
+class Student_short
+  attr_reader :id, :surname_initials, :git, :contact
+
+  def initialize(student)
+    @id = student.id
+    @surname_initials = "#{student.surname} #{student.name[0]}.#{student.patronymic ? " #{student.patronymic[0]}." : ''}"
+    @git = student.get_git
+    @contact = student.contact_info_present? ? student.contact_info : "Нет контактной информации"
+  end
+
+  def self.from_string(id, info_string)
+    data = info_string.split(",").map(&:strip)
+    raise ArgumentError, "Неверный формат строки" if data.size < 4
+
+    surname = data[0]
+    name = data[1]
+    patronymic = data[2]
+    git = data[3]
+    contact = data[4] if data.size > 4
+
+    student = Student.new(surname: surname, name: name, patronymic: patronymic, git: git)
+    student.set_contacts(phone: contact) if contact
+    new(student)
+  end
+end
+
+# ===========
+# напоминалка
+# ===========
+# 
+# по хорошему создать класс person с общими полями и методами,
+# а классы Student и Student_short должны наследоваться от него
+# это как по мне более правильный рефакторинг будет
+# нежели просто убрать дублирование кода
+# -----------
+# обсудить с преподователем задание рефакторинга кода
+# -----------
+# вынести класс Student_short в отдельный файл
+
+# а теперь я усталь, я пошёль спать, и вообще, у меня лапки а не вот это вот всё, вот
