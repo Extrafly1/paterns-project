34663ec648bd312e394a50beaf1ab1dd21dc7519
Efl <133080730+Extrafly1@users.noreply.github.com>
2024-09-21 13:26:46 +0300

    Update and rename userNameARGV.rb to user_name_argv.rb

---
+++ лаболаторная работа 1, введение/user_name_argv.rb
@@ -0,0 +1,33 @@
+if ARGV.empty?                    # если нет аргументов
+  puts "Please enter your name"   # выведем следующее сообщение
+  exit                            # выход, добавил чтоб попробовать как прерывается код
+else                              # иначе
+  name = ARGV[0]                  # присвоим переменной name значение первого аргумента
+  puts "Hello #{name}"            # выведем приветствие
+end
+ARGV.clear                        # очистим массив ARGV
+
+case lang
+when "ruby"
+  puts "Всё понятно, подлиза"
+when "python"
+  puts "Скажи ещё, что тебе 5 лет"
+when "java"
+  puts "Мог бы подумать по лучше"
+when "c++"
+  puts "Любишь крыс" 
+when "c#"
+  puts "А чё не C++?" 
+when "go"
+  puts "А чё не Java?"
+when "kotlin"
+  puts "А чё не Python?"
+when "php"
+  puts "Разлюбишь скоро)"
+else
+  puts "Я тебя не понимаю"
+end
+
+if lang != "ruby"
+puts "в любом случае скоро будет Ruby, и это не обсуждается"
+end
