b3c74cd7ee40cb4260c2dd65813a8bb22ab0284f
Extrafly1 <extrafly555367@gmail.com>
2024-11-09 00:03:00 +0300

    v11_lab3_work1_task47

--- array_processing/array_processing.rb
+++ array_processing/array_processing.rb
@@ -14,4 +14,9 @@ def find_closest_element(array, target)
   array.min_by { |element| (element - target).abs }
 end
 
+# Задача 4: Построить список всех положительных делителей без повторений
+def find_positive_divisors(array)
+  divisors = array.flat_map { |number| (1..number).select { |i| number % i == 0 } if number.positive? }
+  divisors.uniq
+end
 
