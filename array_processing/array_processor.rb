14e31569c9644549ae2cbd9327179728a4c1f881
Extrafly1 <extrafly555367@gmail.com>
2024-11-09 01:42:42 +0300

    v11_lab3_work2_task23

--- array_processing/array_processor.rb
+++ array_processing/array_processor.rb
@@ -11,6 +11,18 @@ class ArrayProcessor
     nil
   end
 
+  # Метод для нахождения минимального элемента по условию
+  def min_by(&block)
+    return nil if @array.empty?
+
+    min_element = @array[0]
+    @array.each do |element|
+      min_element = element if block.call(element) < block.call(min_element)
+    end
+    min_element
+  end
+
+  # Метод для получения массива
   def to_a
     @array
   end
@@ -20,4 +32,5 @@ processor = ArrayProcessor.new([1, 2, 3, 4, 5])
 
 # Пример использования методов
 puts processor.find { |x| x > 3 }            # Output: 4
+puts processor.min_by { |x| -x }              # Output: 5
 
